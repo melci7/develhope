2ea53d2d1a0eb8a3559c7222a6c1f377cfcd2075
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-05 23:17:27 +0300

    exercise20 added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,25 @@
 function App() {
 
     
-    function Welcome({ nameValue, age }) {
+    function Welcome({ nameValue }) {
         return (
             <>
                 <p>Welcome, {nameValue}!</p>
-                <p>Your age is {age}</p>
+                <Age age={24} />
             </>
         )
+        function Age({ age }) {
+            return (
+                <>
+                    <p>Your age is {age}</p>
+                </>
+            )
+        }
     }
     return (
       <>
         {/* JSX Expressions works too as a value */}
-        <div><Welcome nameValue={<strong>Muhammed</strong>} age={24}/></div>
+        <div><Welcome nameValue={<strong>Muhammed</strong>}/></div>
       </>
     )
     
