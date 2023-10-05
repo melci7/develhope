44146101bb58b38d0851a76cc625c9c283349deb
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-05 23:10:38 +0300

    exercise18 added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,17 +1,18 @@
 function App() {
 
     {/* We can give default value to the variable like this. */}
-    function Welcome({ nameValue="Muhammed" }) {
+    function Welcome({ nameValue, age }) {
         return (
             <>
                 <p>Welcome, {nameValue}!</p>
+                <p>Your age is {age}</p>
             </>
         )
     }
     return (
       <>
         {/* If there's no value in the component it just display "Welcome, !" */}
-        <div><Welcome /></div>
+        <div><Welcome nameValue="Muhammed" age={24}/></div>
       </>
     )
     
