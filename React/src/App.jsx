3cbf412d472c5abb64854fa2091bf999b358e36a
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-06 14:33:42 +0300

    exercise18 updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,17 +1,9 @@
+import Welcome from "./Welcome"
+
 function App() {
 
-    {/* We can give default value to the variable like this. */}
-    function Welcome({ nameValue, age }) {
-        return (
-            <>
-                <p>Welcome, {nameValue}!</p>
-                <p>Your age is {age}</p>
-            </>
-        )
-    }
     return (
       <>
-        {/* If there's no value in the component it just display "Welcome, !" */}
         <div><Welcome nameValue="Muhammed" age={24}/></div>
       </>
     )
---
+++ React/src/Welcome.jsx
@@ -0,0 +1,12 @@
+
+
+function Welcome({ nameValue, age }) {
+    return (
+        <>
+            <p>Welcome, {nameValue}!</p>
+            <p>Your age is {age}</p>
+        </>
+    )
+}
+
+export default Welcome
