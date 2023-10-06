68021562c3a3c0bb7e1a42cb09d2fdf43c268e32
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-06 14:26:49 +0300

    exercise14 updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,26 +1,12 @@
-function App() {
+import Message from "./Message"
 
-    function Hello() {
-        return (
-            <>
-                <h2>Hello, World!</h2>
-                <Message />
-            </>
-        )
-        function Message() {
-            return (
-                <>
-                    <p>What a beautiful day!</p>
-                </>
-            )
-        }
-    }
-    return (
-      <>
-        {/* Because of my Message component inside the Hello component, It's not displaying the message. */}
-        <div><Message /></div>
-      </>
-    )
-    
-  }
+function App() {
+  return (
+    <>
+      {/* Displays the message. */}
+      <div><Message /></div>
+    </>
+  )
+}
+
 export default App
