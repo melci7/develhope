2348cfc8d69b0b44ef95d235c3e3b5e12abac548
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-06 14:21:12 +0300

    exercise12 updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,25 +1,11 @@
-function App() {
+import Hello from "./Hello"
 
-    function Hello() {
-        return (
-            <>
-                <h2>Hello, World!</h2>
-                <Message />
-            </>
-        )
-        function Message() {
-            return (
-                <>
-                    <p>What a beautiful day!</p>
-                </>
-            )
-        }
-    }
-    return (
-      <>
-        <div><Hello /></div>
-      </>
+function App() {
+  return (
+    <>
+      <div><Hello /></div>
+    </>
     )
+}
     
-  }
 export default App
---
+++ React/src/Message.jsx
@@ -0,0 +1,9 @@
+function Message() {
+    return (
+        <>
+            <p>What a beautiful day!</p>
+        </>
+    )
+}
+
+export default Message
