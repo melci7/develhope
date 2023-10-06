104ed04668933c7a06a1e917d65fb9b34cd159c4
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-06 14:24:08 +0300

    exercise13 updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,20 +1,7 @@
+import Hello from "./Hello"
+
 function App() {
 
-    function Hello() {
-        return (
-            <>
-                <h2>Hello, World!</h2>
-                <Message />
-            </>
-        )
-        function Message() {
-            return (
-                <>
-                    <p>What a beautiful day!</p>
-                </>
-            )
-        }
-    }
     return (
       <>
         {/* Displayed according to how many times the component has been called */}
