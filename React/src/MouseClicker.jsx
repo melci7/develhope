2d07bc9d57b8258e25dd228fa8546a3288360547
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-10 11:01:15 +0300

    exercise38 added

---
+++ React/src/MouseClicker.jsx
@@ -0,0 +1,17 @@
+function MouseClicker() {
+    
+    // Yes "name" attribute still printed in the console because
+    // image inside the button and button has "event.currentTarget.name" handler.
+    function handleClick(event) {
+        console.log(event.currentTarget.name);
+    }
+    function getSrc(event) {
+        console.log(event.target.src)
+    }
+    return (
+        <button name="one" onClick={handleClick}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="" onClick={getSrc}/> Click</button>
+    )
+}   
+
+export default MouseClicker
