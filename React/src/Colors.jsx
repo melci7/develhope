6ac21a4c3e2212c77178804597eaef896d53e084
Muhammed Emrullah Elçi <melci721@gmail.com>
2023-10-17 12:25:13 +0300

    exercise55 added

--- React/src/Colors.jsx
+++ React/src/Colors.jsx
@@ -1,8 +1,17 @@
+export function Color( {item} ) {
+    return (
+        <li>
+            <p>{item.name}</p>
+        </li>
+    )
+}
+
 export function Colors({ items }) {
     return (
         <div>
             <ul>
-                {items.map(item => (<li key={item.id}>{item.name}</li>))}
+                {items.map(item => 
+                    <Color key={item.id} item={item}/> )}
             </ul>
         </div>
     )
